7b7cf68c55baf7b763682b09800c541ee65affa4
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-06-26 16:03:21 +0300

    Chore: Setup Database
    Our database is basic right now to store users and their sessions

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 require('dotenv').config();
 const express = require('express');
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('./generated/prisma');
+
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000
+    },
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(
+      new PrismaClient(),
+      {
+        checkPeriod: 2 * 60 * 1000,
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  }),
+)
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
